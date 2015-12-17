090c9fe5ac2baa96cee3af38425648e859af4872
unknown <bastien.mottier@gmail.com>
2015-12-17 12:33:08 +0100

    début projet

--- single-projet.php
+++ single-projet.php
@@ -6,14 +6,29 @@
         $img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "full");
         $img = $img[0];
     ?>
-    <div class="postimg" style="<?php if($img){echo 'background:url('.$img.');';} ?>">
+    <div class="postimg" style="<?php if($img){echo 'background-image:url('.$img.');';} ?>">
         <h1 class="title"><?php the_title(); ?></h1>
     </div>
+    <?php $custom = get_post_custom($post->ID); ?>
+    <?php $time=time(); $fin=strtotime($custom['fin_de_la_campagne'][0]);
+        $restant=$time-$fin;
+        if ($restant < 0)
+        {
+            $restant='terminée' ;
+        }
+        else{
+            $restant=$restant/60/60/24;
+            $restant=floor($restant).' jours restants';
+        }
+    ?>
+
+    <?php var_dump($custom); ?>
+
+    <div class="post_stats"><div class='stat-4'><?php echo $restant; ?></div><div class='stat-4'></div><div class='stat-4'></div><div class='stat-4'></div></div>
 
     <div class="content">
         <?php the_content(); ?>
     </div>
-    </article>
 </div>
 <?php endwhile; ?>
 <?php endif; ?> 
